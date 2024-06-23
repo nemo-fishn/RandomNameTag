d3a652b5db81ce509c2ee11827bab37c6da7c3bf
nemo <springmoon0607@gmail.com>
2024-06-23 09:53:17 +0200

    Initial commit for version 1.20.1

---
+++ RNT-1.20.1/src/main/java/net/nemo/random_name_tag/RandomNameTagDataGenerator.java
@@ -0,0 +1,11 @@
+package net.nemo.random_name_tag;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RandomNameTagDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
